f48e68454224641d97f4bf5b43dd02337e7e0b4b
Javohir Khamidullaev <Javohir.py@gmail.com>
2024-12-11 21:03:16 +0500

    Get APIs for Blog model

--- blog/main.py
+++ blog/main.py
@@ -18,22 +18,36 @@ def get_db():
     finally:
         db.close()
 
+
 @app.post('/blog')
 def create_blog(request: Blog, db: Session = Depends(get_db)):
+    """This API can create a blog"""
+
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
-# {
-#     "success": True,
-#     "data": {
-#         "title": request.title,
-#         "body": request.body,
-#         "published": request.published,
-#     }
-# }
+
+@app.get('/blog')
+def get_blogs_list(db: Session = Depends(get_db)):
+    """This API can return all blogs list"""
+
+    blogs = db.query(models.Blog).all()
+    return {"success": True, "data": blogs}
+
+
+@app.get('/blog/{id}')
+def get_blog_detail(id: int, db: Session = Depends(get_db)):
+    """this API can return the blog of that particular ID based on the given ID"""
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if blog:
+        return {"success": True, "data": blog}
+    else:
+        return {"error": f"No blog found with id {id}"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
