8cea77b5c6c76afc5ed145552c5785f73477cfe5
Javohir Khamidullaev <Javohir.py@gmail.com>
2024-12-16 23:00:14 +0500

    Status Code

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Response, HTTPException, status
 
 from blog import models
 from blog.database import engine, SessionLocal
@@ -11,6 +11,7 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
 def get_db():
     db = SessionLocal()
     try:
@@ -19,7 +20,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create_blog(request: Blog, db: Session = Depends(get_db)):
     """This API can create a blog"""
 
@@ -38,15 +39,18 @@ def get_blogs_list(db: Session = Depends(get_db)):
     return {"success": True, "data": blogs}
 
 
-@app.get('/blog/{id}')
-def get_blog_detail(id: int, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get_blog_detail(id: int, response: Response, db: Session = Depends(get_db)):
     """this API can return the blog of that particular ID based on the given ID"""
 
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if blog:
         return {"success": True, "data": blog}
     else:
-        return {"error": f"No blog found with id {id}"}
+        response.status_code=status.HTTP_404_NOT_FOUND
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        # raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No blog found with id {id}")
+        # return {"status_code": response.status_code, "error": f"No blog found with id {id}"}
 
 
 if __name__ == "__main__":
